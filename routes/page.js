3eb5b80a32e095a1493c56bbb18ac2fbf20e515b
Saqibi4213 <mehriamosawer@gmail.com>
2024-10-05 20:10:55 +0430

    added all files

---
+++ routes/page.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+router.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "about.html"));
+});
+
+router.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "contact.html"));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
@@ -8,17 +9,16 @@ const PORT = 3000;
 app.use(bodyParser.json());
 
 // Serve static files from the 'public' directory
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
-// Basic route for the homepage
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-// Import the users route
+// Import the routes
+const indexRoute = require("./routes/index");
+const pageRoute = require("./routes/page");
 const usersRoute = require("./routes/users");
 
-// Use the users route for any routes starting with /users
+// Use the routes
+app.use("/", indexRoute);
+app.use("/", pageRoute);
 app.use("/users", usersRoute);
 
 // Start the server
